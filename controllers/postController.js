d9529bb24deeebc3a22e4dfe8c819e01db5083d4
pietrobertino <pietrobertino20@gmail.com>
2026-04-17 16:06:48 +0200

    complete milestone 4

--- controllers/postController.js
+++ controllers/postController.js
@@ -14,19 +14,13 @@ const index = (req, res) => {
 
 const show = (req, res) => {
     const id = Number(req.params.id);
-    const post = posts.find(post => post.id === id);
-    if (!post) {
 
-        res.status(404);
-
-        return res.json({
-            status: 404,
-            error: "Not Found",
-            message: "Post not found"
-        })
-    }
-
-    res.json(post);
+    const sql = 'SELECT * FROM posts WHERE id = ?';
+    connection.query(sql, [id], (err, results) => {
+        if (err) return res.status(500).json({ error: 'Database query failed' });
+        if (results.length === 0) return res.status(404).json({ error: 'Post not found' });
+        res.json(results[0]);
+    });
 }
 
 const store = (req, res) => {
